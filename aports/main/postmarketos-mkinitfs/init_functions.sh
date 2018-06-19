01bdd4f63ab75895c8b3b2c37ee12055f5984aaa
Oliver Smith <ollieparanoid@bitmessage.ch>
2018-06-20 00:50:39 +0200

    Don't run dhcpcd on the raspberry pi
    Adds deviceinfo_disable_dhcpcd and sets it to true for the pi.

--- aports/main/postmarketos-mkinitfs/init_functions.sh
+++ aports/main/postmarketos-mkinitfs/init_functions.sh
@@ -241,6 +241,14 @@ start_udhcpd() {
 	# Only run once
 	[ -e /etc/udhcpd.conf ] && return
 
+	# Skip if disabled
+	# shellcheck disable=SC2154
+	if [ "$deviceinfo_disable_dhcpcd" = "true" ]; then
+		echo "NOTE: start of dhcpcd is disabled (deviceinfo_disable_dhcpcd)"
+		touch /etc/udhcpcd.conf
+		return
+	fi
+
 	# Get usb interface
 	INTERFACE=""
 	ifconfig rndis0 "$IP" && INTERFACE=rndis0
